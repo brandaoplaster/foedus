e7668e48fdc8a5a74b7e29c57dd2cb505e319a3e
brandaoplaster <brandaoplaster@gmail.com>
2025-08-22 19:06:15 +1200

    feat: Update router with dashboard

--- lib/foedus_web/router.ex
+++ lib/foedus_web/router.ex
@@ -68,6 +68,8 @@ defmodule FoedusWeb.Router do
       on_mount: [{FoedusWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+
+      live "/dashboard", DashboardLive.Index
     end
   end
 
