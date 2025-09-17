c75308f8554a61cb8a40851d8eab76871bab52d1
brandaoplaster <brandaoplaster@gmail.com>
2025-09-17 22:17:15 +1200

    feat: Add schema address

---
+++ lib/foedus/contractors/address.ex
@@ -0,0 +1,35 @@
+defmodule Foedus.Contractors.Address do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Foedus.Contractors.Contractor
+
+  @cast_fields ~w(address_type street number complement neighborhood
+                city state zipcode country contractor_id)a
+
+  @required_fields @cast_fields -- ~w(complement)a
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "addresses" do
+    field :address_type, Ecto.Enum, values: [:residential, :commercial, :billing]
+    field :street, :string
+    field :number, :string
+    field :complement, :string
+    field :neighborhood, :string
+    field :city, :string
+    field :state, :string
+    field :zipcode, :string
+    field :country, :string
+
+    belongs_to :contractor, Contractor
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(address, attrs) do
+    address
+    |> cast(attrs, @cast_fields)
+    |> validate_required(@required_fields)
+  end
+end
