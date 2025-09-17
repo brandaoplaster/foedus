fad2343d41164d52191bf2f78f9b2f79a40e713a
brandaoplaster <brandaoplaster@gmail.com>
2025-09-17 22:50:08 +1200

    feat: Add factory address

---
+++ test/support/factories/address_factory.ex
@@ -0,0 +1,22 @@
+defmodule Foedus.AddressFactory do
+  alias Foedus.Contractors.Address
+
+  defmacro __using__(_opts) do
+    quote do
+      def address_factory do
+        %Address{
+          address_type: Enum.random([:residential, :commercial, :billing]),
+          street: Faker.Address.street_name(),
+          number: Faker.random_between(1, 9999) |> to_string(),
+          complement: sequence(:complement, ["Apto 101", "Sala 205", "Bloco A", nil]),
+          neighborhood: Faker.Address.secondary_address(),
+          city: Faker.Address.city(),
+          state: Faker.Address.state(),
+          zipcode: Faker.Address.zip_code(),
+          country: Faker.Address.country(),
+          contractor: Enum.random([:individual_contractor, :company_contractor]) |> build()
+        }
+      end
+    end
+  end
+end
