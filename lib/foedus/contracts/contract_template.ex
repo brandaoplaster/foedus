ef89c49d31e2397224d6c55cf06fc7e0a743e89f
brandaoplaster <brandaoplaster@gmail.com>
2025-09-17 22:23:03 +1200

    refactor: Refactoring schema contract template

--- lib/foedus/contracts/contract_template.ex
+++ lib/foedus/contracts/contract_template.ex
@@ -2,11 +2,17 @@ defmodule Foedus.Contracts.ContractTemplate do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Foedus.Accounts.User
+
+  @required_fields ~w(title content user_id)a
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "contract_templates" do
     field :title, :string
     field :content, :string
+
+    belongs_to :user, User
 
     timestamps(type: :utc_datetime)
   end
@@ -14,7 +20,10 @@ defmodule Foedus.Contracts.ContractTemplate do
   @doc false
   def changeset(contract_template, attrs) do
     contract_template
-    |> cast(attrs, [:title, :content])
-    |> validate_required([:title, :content])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:title, min: 3, max: 255)
+    |> validate_length(:content, min: 100)
+    |> assoc_constraint(:user)
   end
 end
