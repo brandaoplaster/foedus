230433e8347f976622e12cc8e6d12713f9d0e60e
brandaoplaster <brandaoplaster@gmail.com>
2025-09-24 16:47:50 +1200

    feat: Add view show for contractor

---
+++ lib/foedus_web/live/contractor_live/show.ex
@@ -0,0 +1,22 @@
+defmodule FoedusWeb.ContractorLive.Show do
+  use FoedusWeb, :live_view
+
+  alias Foedus.Contractors
+  import FoedusWeb.Ui.Breadcrumb
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:contractor, Contractors.get_contractor_with_associations!(id))}
+  end
+
+  defp page_title(:show), do: "Show Contractor"
+  defp page_title(:edit), do: "Edit Contractor"
+end
