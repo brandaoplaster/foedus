4da4e869392d772381e1e04fa74de8aeabca0942
brandaoplaster <brandaoplaster@gmail.com>
2025-10-14 15:08:20 +1300

    feat: Add factory signer

---
+++ test/support/factories/signer_factory.ex
@@ -0,0 +1,20 @@
+defmodule Foedus.SignerFactory do
+  alias Foedus.Contracts.Signer
+
+  defmacro __using__(_opts) do
+    quote do
+      def signer_factory do
+        %Signer{
+          role: Enum.random([:legal, :autorizado]),
+          name: Faker.Person.first_name(),
+          status: Enum.random([true, false]),
+          lastname: Faker.Person.last_name(),
+          document: Faker.format("###.###.###-##"),
+          birthdate: Faker.Date.date_of_birth(18..80),
+          email: Faker.Internet.email(),
+          company: build(:company)
+        }
+      end
+    end
+  end
+end
