b2f6bdf780d63cfb220ee2d802707f99ffe04e95
brandaoplaster <brandaoplaster@gmail.com>
2025-10-14 20:35:58 +1300

    feat: Add signers listing

---
+++ lib/foedus_web/live/signer_live/index.ex
@@ -0,0 +1,30 @@
+defmodule FoedusWeb.SignerLive.Index do
+  use FoedusWeb, :live_view
+
+  import FoedusWeb.Components.UI.Table
+
+  alias Foedus.Contracts
+  alias Foedus.Contracts.Signer
+
+  def mount(_params, _session, socket) do
+    signers = Contracts.list_signers()
+    changeset = Contracts.change_signer(%Signer{}, %{})
+
+    socket =
+      socket
+      |> assign(:form, to_form(changeset))
+      |> stream(:signers, signers)
+
+    {:ok, assign(socket, :page_title, "Signers")}
+  end
+
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Signers")
+    |> assign(:signer, nil)
+  end
+end
